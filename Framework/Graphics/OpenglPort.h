a7c9e63bb550254d266cfbc7c97aeb175f0dcb1f
MajakTheCoder-swapp <szymon@swapp-games.com>
2014-03-23 00:57:51 +0100

    target conditional

--- Framework/Graphics/OpenglPort.h
+++ Framework/Graphics/OpenglPort.h
@@ -1,12 +1,9 @@
-#ifdef _WIN32
+#if _WIN32
 #	include <gl/glew.h>
 #elif _TIZEN
 #	include <FGraphicsOpengl2.h>
 	using namespace Tizen::Graphics::Opengl;
-#elif TARGET_OS_IPHONE
+#elif __APPLE__
 #import <OpenGLES/ES2/gl.h>
 #import <OpenGLES/ES2/glext.h>
 #endif
-
-#import <OpenGLES/ES2/gl.h>
-#import <OpenGLES/ES2/glext.h>
