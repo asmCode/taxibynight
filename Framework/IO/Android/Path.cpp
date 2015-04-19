e27be7364ad048b8cb883c21b13e1ce1f955a346
majak <majakthecoder@gmail.com>
2015-04-19 03:07:30 +0200

    ignor na Bunnies, wstepna implementacja Path.cpp

--- Framework/IO/Android/Path.cpp
+++ Framework/IO/Android/Path.cpp
@@ -1,6 +1,9 @@
 #ifdef __ANDROID__
 
 #include "../Path.h"
+#include <dirent.h>
+
+// TODO: dokonczyc implementacje!!
 
 bool Path::IsFileExists(const char *fileName)
 {
@@ -11,6 +14,11 @@ void Path::GetAllFiles(std::vector<std::string> &files,
 					   const std::string &path,
 					   const std::string &filter)
 {
+	dirent** filenames;
+	int count = scandir(path.c_str(), &filenames, NULL, alphasort);
+
+	for (int i = 0; i < count; i++)
+		files.push_back(filenames[i]->d_name);
 }
 
 #endif
