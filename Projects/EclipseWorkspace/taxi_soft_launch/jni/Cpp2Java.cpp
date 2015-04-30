f9ff3c2c6c6591732582c653fd9fefc92646f74d
majak <majakthecoder@gmail.com>
2015-04-30 03:41:03 +0200

    obsluga analityki po stronie c++

--- Projects/EclipseWorkspace/taxi_soft_launch/jni/Cpp2Java.cpp
+++ Projects/EclipseWorkspace/taxi_soft_launch/jni/Cpp2Java.cpp
@@ -12,6 +12,21 @@ void Cpp2Java::RequestAppQuit()
 	CallStaticMethodV("RequestAppClose");
 }
 
+void Cpp2Java::LogAnalyticsEvent(const std::string& name)
+{
+	CallStaticMethodV("LogAnalyticsEvent", name);
+}
+
+void Cpp2Java::LogAnalyticsEvent(const std::string& name, const std::string& value)
+{
+	CallStaticMethodV("LogAnalyticsEvent", name, value);
+}
+
+void Cpp2Java::LogAnalyticsEvent(const std::string& name, float value)
+{
+	CallStaticMethodV("LogAnalyticsEvent", name, value);
+}
+
 void Cpp2Java::CallStaticMethodV(const std::string& methodName)
 {
 	JNIEnv* env;
@@ -25,5 +40,57 @@ void Cpp2Java::CallStaticMethodV(const std::string& methodName)
 	jmethodID methodId = env->GetStaticMethodID(clazz, methodName.c_str(), "()V");
 
 	env->CallStaticVoidMethod(clazz, methodId);
+}
+
+void Cpp2Java::CallStaticMethodV(const std::string& methodName, const std::string& param)
+{
+	JNIEnv* env;
+	if (javaVM->GetEnv((void**)&env, JNI_VERSION_1_6) != JNI_OK)
+	{
+		LOGE("CallStaticMethodV: cannot get env");
+		return;
+	}
+
+	jclass clazz = env->FindClass("com/ssg/taxisoftlaunch/GL2JNILib");
+	jmethodID methodId = env->GetStaticMethodID(clazz, methodName.c_str(), "(Ljava/lang/String;)V");
+
+	jstring javaParam = env->NewStringUTF(param.c_str());
+
+	env->CallStaticVoidMethod(clazz, methodId, javaParam);
+}
+
+void Cpp2Java::CallStaticMethodV(const std::string& methodName, const std::string& param1, const std::string& param2)
+{
+	JNIEnv* env;
+	if (javaVM->GetEnv((void**)&env, JNI_VERSION_1_6) != JNI_OK)
+	{
+		LOGE("CallStaticMethodV: cannot get env");
+		return;
+	}
+
+	jclass clazz = env->FindClass("com/ssg/taxisoftlaunch/GL2JNILib");
+	jmethodID methodId = env->GetStaticMethodID(clazz, methodName.c_str(), "(Ljava/lang/String;Ljava/lang/String;)V");
+
+	jstring javaParam1 = env->NewStringUTF(param1.c_str());
+	jstring javaParam2 = env->NewStringUTF(param2.c_str());
+
+	env->CallStaticVoidMethod(clazz, methodId, javaParam1, javaParam2);
+}
+
+void Cpp2Java::CallStaticMethodV(const std::string& methodName, const std::string& param1, float param2)
+{
+	JNIEnv* env;
+	if (javaVM->GetEnv((void**)&env, JNI_VERSION_1_6) != JNI_OK)
+	{
+		LOGE("CallStaticMethodV: cannot get env");
+		return;
+	}
+
+	jclass clazz = env->FindClass("com/ssg/taxisoftlaunch/GL2JNILib");
+	jmethodID methodId = env->GetStaticMethodID(clazz, methodName.c_str(), "(Ljava/lang/String;F)V");
+
+	jstring javaParam1 = env->NewStringUTF(param1.c_str());
+
+	env->CallStaticVoidMethod(clazz, methodId, javaParam1, param2);
 }
 
--- Projects/EclipseWorkspace/taxi_soft_launch/jni/Cpp2Java.h
+++ Projects/EclipseWorkspace/taxi_soft_launch/jni/Cpp2Java.h
@@ -7,12 +7,14 @@ class Cpp2Java
 {
 public:
 	static void RequestAppQuit();
+	static void LogAnalyticsEvent(const std::string& name);
+	static void LogAnalyticsEvent(const std::string& name, const std::string& value);
+	static void LogAnalyticsEvent(const std::string& name, float value);
 
 private:
-	/*static JNIEnv* m_env;
-	static jclass m_clazz;
-	static jmethodID m_requestAppQuitId;*/
-
 	static void CallStaticMethodV(const std::string& methodName);
+	static void CallStaticMethodV(const std::string& methodName, const std::string& param);
+	static void CallStaticMethodV(const std::string& methodName, const std::string& param1, const std::string& param2);
+	static void CallStaticMethodV(const std::string& methodName, const std::string& param1, float param2);
 };
 
