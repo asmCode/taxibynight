19e0bfb8f0cac6385b398963b8ea3a0cec869db7
majak <majakthecoder@gmail.com>
2015-05-02 20:44:17 +0200

    DummyAnalytics loguje do konsoli

--- Bunnies/DummyAnalytics.cpp
+++ Bunnies/DummyAnalytics.cpp
@@ -1,5 +1,6 @@
 #include "DummyAnalytics.h"
 #include <Analytics/AnalyticsEvent.h>
+#include <Utils/Log.h>
 
 void DummyAnalytics::SetUser(const std::string& userId)
 {
@@ -7,5 +8,19 @@ void DummyAnalytics::SetUser(const std::string& userId)
 
 void DummyAnalytics::TrackEvent(const AnalyticsEvent& event)
 {
+	switch (event.GetParamType())
+	{
+	case AnalyticsEvent::ParamType_None:
+		Log::LogT("Logging event %s", event.GetEventName().c_str());
+		break;
+
+	case AnalyticsEvent::ParamType_String:
+		Log::LogT("Logging event %s with param %s", event.GetEventName().c_str(), event.GetParamString().c_str());
+		break;
+
+	case AnalyticsEvent::ParamType_Float:
+		Log::LogT("Logging event %s with param %f", event.GetEventName().c_str(), event.GetParamFloat());
+		break;
+	}
 }
 
